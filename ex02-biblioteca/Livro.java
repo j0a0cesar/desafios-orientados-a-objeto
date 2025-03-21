695c5a5f34a88bd43d678e3333e322c7678089a1
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-21 10:57:45 -0300

    Create Livro.java

---
+++ ex02-biblioteca/Livro.java
@@ -0,0 +1,15 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public String getTitulo(String titulo){
+        return titulo;
+    }
+    public String getAutor(String autor){
+        return autor;
+    }
+    public int getAno(int ano){
+        return ano;
+    }
+}
