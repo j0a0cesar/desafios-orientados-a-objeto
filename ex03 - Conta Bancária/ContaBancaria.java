d7ad22e4c4aeaa8e1555f17d2500d426e8a9870f
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-03-24 11:14:53 -0300

    Create ContaBancaria.java

---
+++ ex03 - Conta Bancária/ContaBancaria.java
@@ -0,0 +1,16 @@
+public class ContaBancaria {
+    private int numeroConta;
+    private String nomeTitular;
+    private float saldo;
+
+
+    public depositarValor(){
+
+    }
+    public sacarValor(){
+
+    }
+    public verSaldo(){
+
+    }
+}
