46136fc2f081d9f8190181b331c6677dba960da5
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-03-24 19:22:37 -0300

    Update ContaBancaria.java

--- ex03 - Conta Bancária/ContaBancaria.java
+++ ex03 - Conta Bancária/ContaBancaria.java
@@ -1,16 +1,44 @@
 public class ContaBancaria {
     private int numeroConta;
     private String nomeTitular;
-    private float saldo;
+    private double saldo;
 
-
-    public depositarValor(){
-
+    public ContaBancaria(int numeroConta, String nomeTitular, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = saldoInicial;
     }
-    public sacarValor(){
 
+    //getters
+    public int getNumeroConta() {
+        return numeroConta;
     }
-    public verSaldo(){
 
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    //getter para visualizar saldo
+    public double getSaldo() {
+        return saldo;
+    }
+
+    //métodos para depositar, sacar
+    public void depositar(double deposito){
+        if(deposito > 0){
+            saldo = saldo + deposito;
+            System.out.println("Depósito de R$" + deposito + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor inválido.");
+        }
+    }
+
+    public void sacar(double saque){
+        if(saque > 0 && saque <= saldo){
+            saldo = saldo - saque;
+            System.out.println("Saque de R$" + saque + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor inválido.");
+        }
     }
 }
