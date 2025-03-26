3982b689c68b8d2f828d1b2f579350825c2514a4
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-26 16:58:18 -0300

    Update Agenda.java

--- ex06 -  Agenda/Agenda.java
+++ ex06 -  Agenda/Agenda.java
@@ -1,33 +1,49 @@
 import java.util.ArrayList;
-
+import java.util.Objects;
 
 public class Agenda {
-    ArrayList<Contato> contatos = new ArrayList<>();
 
+    Contato[] contatos = new Contato[5];
+    int count = 0;
 
     public void adicionarContato(Contato novoContato) {
-        contatos.add(novoContato);
-        System.out.println("Novo contato salvo!");
+
+        if (count < 5) {
+            contatos[count] = novoContato;
+            count++;
+            System.out.println("Novo contato salvo!");
+        } else {
+            System.out.println("Agenda cheia!Não é mais possível adicionar mais contatos.");
+        }
     }
 
     public Contato buscarContato(String nomeProcurado) {
 
-        for (Contato contato : contatos) {
-            if (contato.getNome().equalsIgnoreCase(nomeProcurado)) {
-                return contato;
+        for (int i = 0; i < count; i++) {
+            if (Objects.equals(nomeProcurado, contatos[i].getNome())) {
+                return contatos[i];
             }
         }
-            System.out.println("Contato não encontrado!");
-            return null;
+        System.out.println("Contato não encontrado!");
+        return null;
     }
-    public boolean removerContato(String nome) {
-        Contato c = buscarContato(nome);
-        if(c != null) {
-            contatos.remove(c);
-            return true;
+    public boolean removerContato(String nomeProcurado) {
+
+        for (int i = 0; i < count; i++) {
+            if (Objects.equals(nomeProcurado, contatos[i].getNome())) {
+                for(int j = i; j < count - 1;j ++){
+                    contatos[j] = contatos[j+1];
+                }
+                contatos[count-1] = null;
+                count--;
+                System.out.print("Contato removido com sucesso!");
+                return true;
+            }
         }
+        System.out.println("Contato não encontrado!");
         return false;
     }
+
 }
 
 
