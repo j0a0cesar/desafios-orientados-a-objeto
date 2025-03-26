8cf96aa79657da18b7bed06a7610b4a274135b49
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-26 20:37:51 -0300

    Create Pizza.java

---
+++ ex08 - Pizzaria/Pizza.java
@@ -0,0 +1,18 @@
+public class Pizza {
+    private String nome;
+    private double valor;
+
+
+        public Pizza(String nome, double valor){
+            this.nome = nome;
+            this.valor = valor;
+        }
+        public String getNome(){
+            return this.nome;
+        }
+        public double getValor(){
+            return this.valor;
+        }
+
+
+}
